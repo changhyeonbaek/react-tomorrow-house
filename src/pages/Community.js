5f5ff1ea99cf1e9564cf2f0e7cee8418004d4f28
Changhyeon Baek <sabondev@gmail.com>
2025-04-02 12:54:12 +0900

    Add Community component in Community.js: implement community page with CommunityShow and modal support

---
+++ src/pages/Community.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import CommunityShow from '../components/CommunityShow'
+
+const Community = ({ openModal }) => {
+  return (
+    <>
+      <CommunityShow />
+    </>
+  )
+}
+
+export default Community
