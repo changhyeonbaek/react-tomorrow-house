b5ff6b111642eb8c2d2ab679fbdc689221d665da
Changhyeon Baek <sabondev@gmail.com>
2025-04-02 13:03:54 +0900

    Initialize App.js with BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './styles/main.scss';
 
 function App() {
   return (
-    <></>
+    <Router>
+    </Router>
   );
 }
 
